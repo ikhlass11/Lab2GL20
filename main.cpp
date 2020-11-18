5c5f8216b6c12d755edd71dd300364996b4e2a81
Limouna <58744759+Limouna@users.noreply.github.com>
2020-11-18 23:12:14 +0100

    Changed main function

--- main.cpp
+++ main.cpp
@@ -1,15 +1,25 @@
 #include <iostream>
 #include <string>
-
 #include "hash.h"
 
 using namespace std;
 
 int main()
 {
-    hash1 hashObj;
-    int index;
-    index = hashObj.HASH("Kerzazi");
+    hash1 HT;
+    
+    HT.ajouterItem("Allo", "064132878");
+    HT.ajouterItem("Test", "074864287");
+    HT.ajouterItem("Ikram", "064513287");
+    HT.ajouterItem("Ayoub", "054961232");
+    HT.ajouterItem("Fatima", "067894315");
+    HT.ajouterItem("Ikhlass", "064295432");
+    HT.ajouterItem("Ayman", "06948623");
+    HT.ajouterItem("cd", "06786123");
+    HT.ajouterItem("be", "05379861"); //same key as the previous one, to check
+
+
+    HT.afficherItem();
 
 
     return 0;
