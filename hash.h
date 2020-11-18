64ee24d36db998c8ac3edd24156cd4d8ea9a598f
Limouna <58744759+Limouna@users.noreply.github.com>
2020-11-18 23:10:37 +0100

    AjouterItem et AfficherItem

--- hash.h
+++ hash.h
@@ -1,6 +1,7 @@
 #include <iostream>
+#include <list>
+#include <cstring>
 #include <string>
-
 using namespace std;
 
 #ifndef HASH_H_INCLUDED
@@ -9,36 +10,13 @@ using namespace std;
 class hash1{
     private:
         static const int tailleTableau = 10;
+        list<pair<string,string>> table[tailleTableau];
 
-        struct item{
-                string nom;
-                string tel;
-                item* next;
-        };
-
-        item* HashTable[tailleTableau];
 
     public:
-        hash1();
-        int HASH(string cle);
-
-        // les fonctions à developper
-
-
-        //void ajouterItem(string nom, string tel);
-        //void afficherItem();
-
-        //int  nombreItems(int index);
-        //void afficherItemsDansIndex(int index); // les items qui ont le même resultat de hashage et qui sont empilés
-        //void chercherItem(string nom);
-        //void supprimerItem(string nom);
-
-//        Un problème d’un entrevue chez Google.
-//        Soit une fonction qui prend comme paramètre une chaine de caractères et retourne le premier caractère récurrent dans la chaine.
-//        “ABCA”    ‘A’
-//        “BCABA” –>  ‘B’
-//        “ABC”  NULL
-
+        int HASH(string cle);  //Done
+        void ajouterItem(string nom, string tel); //Done
+        void afficherItem(); //Done
 
 };
 
