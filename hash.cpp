04acb34f5c4063fd08b402b2a7d69d9bf25a1bfd
[ikhlass11] <ikhlass_loukili@hotmail.com>
2020-11-18 23:09:51 +0100

    Update hash.cpp

--- hash.cpp
+++ hash.cpp
@@ -8,32 +8,23 @@ int hash1::HASH(string cle)
 {
     int index;
     int hashage = 0;
-
-    cout << cle.length();
-    //for sur le nombre de char de cle
     for(int i = 0; i< cle.length(); i++)
     {
-        // pour chaque char je reccupere le code ascii et je l'ajoute a la somme
         hashage = hashage + (int)cle[i];
-        cout << cle[i] << endl;
-        cout << hashage << endl;
     }
-
-    // modulo de la sommme des codes ascii sur tailleTableau
-
     index = hashage % tailleTableau;
-
-    //index = 0;
-    cout << index << endl;
 
     return index;
 }
 
-hash1::hash1(){
-     for (int i = 0; i<tailleTableau; i++){
-        HashTable[i] = new item;
-        HashTable[i]->nom = "vide";
-        HashTable[i]->tel = "vide";
-        HashTable[i]->next = NULL;
-     }
+int hash1::nombreItems(int index) {
+    int nbreItem{};
+    for (int i{}; i<tailleTableau; i++) {
+        if (table[i].size() == 0) continue;
+        
+        auto bItr = table[i].begin();
+        for (; bItr != table[i].end(); bItr++) nbreItem++;
+    }
+    return nbreItem;
 }
+
